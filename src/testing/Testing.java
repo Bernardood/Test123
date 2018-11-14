6f536b2d51097b566c6256a4cfba785e465114ba
Admin <Admin@10.23.105.163>
2018-11-14 20:03:27 +0000

    Upload broken project

---
+++ src/testing/Testing.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testing;
+
+/**
+ *
+ * @author Admin
+ */
+public class Testing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+      double i = Math.random()* 5 + 1;
+      
+      if (i== 6.0){
+           String a "The Dragon has been slain!";
+      } else if(i== 1){
+               String b "Good try...";
+          
+      }
+      }
+// TODO code application logic here
+    }
+    
+}
